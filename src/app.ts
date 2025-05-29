fec95498be61794ca32f80329ceeee2783f8405b
4ksandrey <62288518+4ksandrey@users.noreply.github.com>
2025-05-29 12:28:07 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -33,11 +33,11 @@ const server = new Drash.Http.Server({
 });
 
 server.run({
-  hostname: "0.0.0.0",
+  hostname: "realworld_drash",
   port: 1667,
 });
 
-console.log("Drash server running on 0.0.0.0:1667");
+console.log("Drash server running on realworld_drash:1667");
 console.log(
   "Navigate to localhost:8080 for a proxy pass, or localhost:1667 to be direct",
 );
